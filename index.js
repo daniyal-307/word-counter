7edfeba134f6544717efe17a49337845aa67a06e
Daniyal Shahzad <daniyal-307@users.noreply.github.com>
2024-04-23 02:24:23 +0500

    Pushing code to Github

---
+++ index.js
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let condition = true;
+while (condition) {
+    const answer = await inquirer.prompt([
+        {
+            name: "Paragraph",
+            type: "input",
+            message: chalk.yellowBright("Please enter your paragraph to count the words and the characters:")
+        }
+    ]);
+    // Storing the input data inside another variable by removing the white space and line terminator characters
+    const words = answer.Paragraph.trim();
+    console.log(`The number of characters in your paragraph are: ${chalk.blueBright(words.length)}`);
+    // Now counting the words instead of single character, by splitting the characters into words
+    const wordsSplit = words.split(" ");
+    console.log(`The number of words in your paragraph are: ${chalk.greenBright(wordsSplit.length)}`);
+    const startAgain = await inquirer.prompt([
+        {
+            name: "restart",
+            default: "true",
+            type: "confirm",
+            messsage: "Do you wish to restart?"
+        }
+    ]);
+    condition = startAgain.restart;
+}
+;
